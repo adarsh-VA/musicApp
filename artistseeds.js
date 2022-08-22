6874dfd7c8d5e7aac612cbaa3620889cc2e6a176
adarsh-VA <aadi.vodnala@gmail.com>
2022-08-22 20:37:23 +0530

    seeds

---
+++ artistseeds.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const db='mongodb://localhost:27017/dotify';
+const artistm = require('./models/artistm');
+
+mongoose.connect(db)
+    .then(()=>{
+        console.log('connection open');
+    })
+    .catch((err)=>{
+        console.log('errr occured',err);
+    })
+
+async function create (){
+    for(let i=1;i<=15;i++){
+        var name = `artist${i}`;
+        var dob = '2022-08-01';
+        var img = '';
+        var bio = 'good singer';
+        const a = new artistm({name,dob,bio,image:img});
+        await a.save();
+    }
+}
+
+create();
---
+++ userseeds.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const db='mongodb://localhost:27017/dotify';
+const userm = require('./models/userm');
+
+mongoose.connect(db)
+    .then(()=>{
+        console.log('connection open');
+    })
+    .catch((err)=>{
+        console.log('errr occured',err);
+    })
+
+
+async function create (){
+    for(let i=1;i<=20;i++){
+        var email = `user${i}@${i}`;
+        var username = `user${i}`;
+        var password = '123';
+        const user = await new userm({email,username});
+        const ruser = await userm.register(user,password);
+    }
+}
+    
+create();
